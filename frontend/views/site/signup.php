a56518a4f2a6c71f2f3e5ed7538c1683fa2d1861
Satorlous <n.leppik@gmail.com>
2019-10-15 23:35:16 +0500

    First commit including some changes

---
+++ frontend/views/site/signup.php
@@ -0,0 +1,35 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Регистрация';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-signup">
+    <h1><?= Html::encode($this->title) ?></h1><br>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+                <?= $form->field($model, 'username')->label('Никнейм')?>
+
+                <?= $form->field($model, 'email')->label('Электронный адрес')?>
+
+                <?= $form->field($model, 'password')->label('Пароль')->passwordInput() ?>
+
+                <?= $form->field($model, 'password_repeat')->label('Подтверждение пароля')->passwordInput() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
